b2af63b5733556e71673c487ae791f74f92841fc
nguyenhuyphuc26112004 <phuc40965@gmail.com>
2024-05-22 09:54:36 +0700

    NHP108_Cau2

--- Bai Kiem Tra/src/NHP108Person.java
+++ Bai Kiem Tra/src/NHP108Person.java
@@ -4,7 +4,29 @@ public class NHP108Person {
     String hoten;
     int tuoi;
     String quequan;
+    
+    public void nhapthongtin(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap ho ten: ");
+        hoten = sc.nextLine();
+        System.out.println("NHap tuoi: ");
+        tuoi = sc.nextInt();
+        System.out.println("Nhap que quan: ");
+        quequan = sc.nextLine();
+    }
 
+    public void xuatthongtin(){
+        System.out.println("Ho ten: " +hoten);
+        System.out.println("Tuoi: " +tuoi);
+        System.out.println("Que quan: ");
+    }
+
+    public static void main(String[] args) {
+        NHP108Person nhp = new NHP108Person();
+        nhp.nhapthongtin();
+        nhp.xuatthongtin();
+    }
+    
     
 
 }
